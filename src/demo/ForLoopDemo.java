05c45be4aeec210881436a362d03b0a6daa1b45c
suranjanp <suranzanrocks@gmail.com>
2021-07-19 19:15:15 -0500

    added ForLoopDemo class

---
+++ src/demo/ForLoopDemo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+	for(int i = 1; i<=5; i++);{
+	String i;
+	System.out.println("hello ....." + i);
+	}
+	}
+}
